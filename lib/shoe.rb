3ba3dcd107dd6279cbbf5b6ad5314189b87534de
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-21 09:40:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,15 @@
 class Shoe
-  attr_accessor
+  attr_accessor :brand, :color, :size, :material, :condition,
 
   def initialize(brand)
     @brand = brand
   end
 
-  def brand 
-    @brand
+  def cobble 
+    puts shoe.condition
   end
+
+
+  
 
 end
